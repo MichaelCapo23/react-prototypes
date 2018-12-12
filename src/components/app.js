bb24fdfd01d9a48c97ef5a942b6f17b46ba190d1
MichaelCapo23 <123pie23@gmail.com>
2018-12-11 20:52:48 -0800

    finshed prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,33 @@
+import React from 'react'
+import Table from './table'
 
+export default () => {
+
+const students = [
+    {
+        name: "Mike",
+        course : "Python",
+        grade: 88
+    },
+    {
+        name: "James",
+        course : "spotify API",
+        grade: 100
+    },
+    {
+        name: "Justen",
+        course : "C++",
+        grade: 97
+    }
+
+];
+
+
+    return (
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+}
 
