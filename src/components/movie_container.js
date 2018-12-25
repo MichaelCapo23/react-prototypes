c36af047c71f40ebccf56a8c01cc538626d44632
MichaelCapo23 <123pie23@gmail.com>
2018-12-24 21:40:02 -0800

    finsihed axios prototype going to go back and do it with a try catch statement

--- src/components/movie_container.js
+++ src/components/movie_container.js
@@ -3,23 +3,46 @@ import Movie from './movies';
 import axios from 'axios';
 
 class MoviesContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            movies: [],
+        }
+    }
 
     async componentWillMount(){
         const url = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json';
-        try {
-            let response = await axios.get(url);
-            con
-        } catch (err) {
+        axios.get(url).then((resp) => {
+            console.log('Resp:', resp);
 
-        }
+            this.setState({
+                movies: resp.data.feed.entry
+            });
+        });
+
+
+
+        // try {
+        //     let response = await axios.get(url);
+        //     console.log('Resp:', response);
+        // } catch (err) {
+        //
+        // }
         
     }
 
     render(){
+        console.log('The State is:', this.state);
+
+        const movieList = this.state.movies.map((movieInfo, index) => {
+            return <Movie info={movieInfo} key={index}/>
+        });
+
+
         return (
             <div>
-                <h2>Movie Container</h2>
-                <Movie/>
+                {movieList}
             </div>
         )
     }
